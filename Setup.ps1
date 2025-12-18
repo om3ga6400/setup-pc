e95137656b4b2f95767334db8e6fabf1e55f5011
Ω <om3ga6400.dev@gmail.com>
2025-12-18 13:23:56 +0000

    Add files via upload

---
+++ Setup.ps1
@@ -0,0 +1,35 @@
+winget install StartAllBack.StartAllBack --scope machine
+
+winget install Proton.ProtonVPN
+winget install Proton.ProtonAuthenticator
+winget install Proton.ProtonPass
+winget install AdGuard.AdGuard
+winget install HiBitSoftware.HiBitUninstaller
+
+winget install Ablaze.Floorp
+winget install Spotify.Spotify
+winget install Discord.Discord
+
+winget install Valve.Steam
+winget install EpicGames.EpicGamesLauncher
+winget install PrismLauncher.PrismLauncher
+
+winget install Microsoft.PowerShell
+winget install TechPowerUp.NVCleanstall
+winget install Notepad++.Notepad++
+
+Invoke-WebRequest "https://github.com/Equicord/Equilotl/releases/latest/download/Equilotl.exe" -OutFile "$env:USERPROFILE\Downloads\Equilotl.exe"
+Start-Process "$env:USERPROFILE\Downloads\Equilotl.exe"
+
+Invoke-WebRequest "https://github.com/voidstrap/Voidstrap/releases/latest/download/Voidstrap.exe" -OutFile "$env:USERPROFILE\Downloads\Voidstrap.exe"
+Start-Process "$env:USERPROFILE\Downloads\Voidstrap.exe"
+
+Invoke-WebRequest "https://github.com/SpotX-Official/SpotX/releases/latest/download/Install_New_theme.bat" -OutFile "$env:USERPROFILE\Downloads\SpotX.bat"
+Start-Process "$env:USERPROFILE\Downloads\SpotX.bat"
+
+Invoke-WebRequest "https://github.com/marticliment/UniGetUI/releases/latest/download/UniGetUI.Installer.exe" -OutFile "$env:USERPROFILE\Downloads\UniGetUI.exe"
+Start-Process "$env:USERPROFILE\Downloads\UniGetUI.exe"
+
+Start-Process "https://github.com/statsfm/specter-releases/tags/"
+
+winget update --all --include-unknown
