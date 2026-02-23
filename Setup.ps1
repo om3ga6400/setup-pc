20f8fab1093422f9a71c80df9ee278e3fe7354ea
Ω <om3ga6400.dev@gmail.com>
2026-02-23 18:03:29 -0500

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -20,23 +20,36 @@ winget install Winaero.Tweaker
 winget install Microsoft.WindowsTerminal
 winget install Microsoft.PowerShell
 winget install Microsoft.PowerShell.Preview
+
 winget install GitHub.GitHubDesktop
 winget install Microsoft.VisualStudioCode
 winget install Git.Git
-winget install Notepad++.Notepad++
 
-Invoke-WebRequest "https://github.com/Equicord/Equilotl/releases/latest/download/Equilotl.exe" -OutFile "$env:USERPROFILE\Downloads\Equilotl.exe"
-Start-Process "$env:USERPROFILE\Downloads\Equilotl.exe"
+winget install PeterPawlowski.Foobar2000
 
-Invoke-WebRequest "https://github.com/voidstrap/Voidstrap/releases/latest/download/Voidstrap.exe" -OutFile "$env:USERPROFILE\Downloads\Voidstrap.exe"
-Start-Process "$env:USERPROFILE\Downloads\Voidstrap.exe"
+winget install Corsair.iCUE.5
+winget install SteelSeries.GG
 
-Start-Process "https://github.com/statsfm/specter-releases/tags/"
+OBSProject.OBSStudio
 
 winget update --all --include-unknown
 
+$DownloadFolder = "$env:USERPROFILE\Desktop\Downloads"
+New-Item -ItemType Directory -Path $DownloadFolder
 
+Invoke-WebRequest "https://github.com/Equicord/Equilotl/releases/latest/download/Equilotl.exe" -OutFile "$DownloadFolder\Equilotl.exe"
+Start-Process "$DownloadFolder\Equilotl.exe"
 
+Invoke-WebRequest "https://github.com/voidstrap/Voidstrap/releases/latest/download/Voidstrap.exe" -OutFile "$DownloadFolder\Voidstrap.exe"
+Start-Process "$DownloadFolder\Voidstrap.exe"
 
+Invoke-WebRequest "https://api.wooting.io/public/wootility/download?os=win" -OutFile "$DownloadFolder\Wootility-Setup.exe"
+Start-Process "$DownloadFolder\Wootility-Setup.exe"
 
+Invoke-WebRequest "https://github.com/okkdev/melty-installer/releases/latest/download/MeltyInstaller.exe" -OutFile "$DownloadFolder\MeltyInstaller.exe"
+Start-Process "$DownloadFolder\MeltyInstaller.exe"
 
+Invoke-WebRequest "https://github.com/mifi/lossless-cut/releases/latest/download/LosslessCut-win-x64.7z" -OutFile "$DownloadFolder\LosslessCut-win-x64.7z"
+& "$env:LOCALAPPDATA\Microsoft\WindowsApps\NanaZipC.exe" x "$DownloadFolder\LosslessCut-win-x64.7z" -o"$DownloadFolder\LosslessCut" -y
+
+Start-Process "https://github.com/statsfm/specter-releases/tags/"
