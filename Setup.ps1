c34067614d11f60e36aa1b6acbeea427c4b8b6d8
Ω <om3ga6400.dev@gmail.com>
2026-06-05 13:59:53 -0700

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -4,13 +4,10 @@ winget install Discord.Discord
 winget install Valve.Steam
 winget install EpicGames.EpicGamesLauncher
 winget install PrismLauncher.PrismLauncher
-winget install Proton.ProtonVPN
 winget install Proton.ProtonAuthenticator
-winget install Proton.ProtonPass
 winget install AdGuard.AdGuard
 winget install HiBitSoftware.HiBitUninstaller
 winget install TechPowerUp.NVCleanstall
-winget install MartiCliment.UniGetUI
 winget install Microsoft.WindowsTerminal
 winget install Microsoft.PowerShell
 winget install GitHub.GitHubDesktop
@@ -21,15 +18,8 @@ winget install SteelSeries.GG
 winget install OBSProject.OBSStudio
 winget install ActivityWatch.ActivityWatch
 
-Start-Process "https://github.com/Equicord/Equilotl/releases/latest/download/Equilotl.exe"
+Start-Process "https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli.exe"
 Start-Process "https://github.com/voidstrap/Voidstrap/releases/latest/download/Voidstrap.exe"
-Start-Process "https://github.com/okkdev/melty-installer/releases/latest/download/MeltyInstaller.exe"
-Start-Process "https://github.com/Devolutions/UniGetUI/releases/download/latest/UniGetUI.Installer.exe"
 Start-Process "https://api.wooting.io/public/wootility/download?os=win"
 Start-Process "https://github.com/geode-sdk/geode/releases/latest"
 Start-Process "https://github.com/mifi/lossless-cut/releases/latest/download/LosslessCut-win-x64.7z"
-
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSecondsInSystemClock" -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value 1
-
-Write-Output 'Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value 0'
