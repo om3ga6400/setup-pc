a2a01f59c227a1e53aaa2fd05f70954b188e0401
om3ga6400 <om3ga6400.dev@gmail.com>
2026-09-07 16:04:30 -0700

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,25 +1,23 @@
-winget install Waterfox.Waterfox
+winget install Mozilla.Firefox
 winget install Spotify.Spotify
 winget install Discord.Discord
 winget install Valve.Steam
 winget install EpicGames.EpicGamesLauncher
 winget install PrismLauncher.PrismLauncher
-winget install Proton.ProtonAuthenticator
-winget install AdGuard.AdGuard
 winget install HiBitSoftware.HiBitUninstaller
 winget install TechPowerUp.NVCleanstall
 winget install Microsoft.WindowsTerminal
 winget install Microsoft.PowerShell
 winget install GitHub.GitHubDesktop
-winget install Microsoft.VisualStudioCode
+winget install VSCodium.VSCodium
 winget install Git.Git
 winget install Corsair.iCUE.5
 winget install SteelSeries.GG
 winget install OBSProject.OBSStudio
-winget install ActivityWatch.ActivityWatch
+winget install Devolutions.UniGetUI
 
-Start-Process "https://github.com/TestcordDev/TestCord/releases/download/latest/Windows_Testcord_installer-rel_cli.exe"
-Start-Process "https://github.com/voidstrap/Voidstrap/releases/latest/download/Voidstrap.exe"
+Start-Process "https://github.com/TestcordDev/TestCord/releases/download/latest/Windows_Testcord_installer-rel.exe"
+Start-Process "https://github.com/KloBraticc/Voidstrap/releases/latest/download/Voidstrap.exe"
 Start-Process "https://api.wooting.io/public/wootility/download?os=win"
 Start-Process "https://github.com/geode-sdk/geode/releases/latest"
 Start-Process "https://github.com/mifi/lossless-cut/releases/latest/download/LosslessCut-win-x64.7z"
