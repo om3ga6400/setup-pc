74b010a9ff89ef1912347351e14ef09002b261d6
om3ga6400 <om3ga6400.dev@gmail.com>
2026-09-07 16:36:09 -0700

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -15,6 +15,7 @@ winget install Corsair.iCUE.5
 winget install SteelSeries.GG
 winget install OBSProject.OBSStudio
 winget install Devolutions.UniGetUI
+winget install Jurplel.qView
 
 Start-Process "https://github.com/TestcordDev/TestCord/releases/download/latest/Windows_Testcord_installer-rel.exe"
 Start-Process "https://github.com/KloBraticc/Voidstrap/releases/latest/download/Voidstrap.exe"
