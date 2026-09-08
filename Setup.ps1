bb9045ac52c740f1afa66cf27e9efde54875b59b
om3ga6400 <om3ga6400.dev@gmail.com>
2026-09-08 16:05:05 -0700

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -21,6 +21,7 @@ winget install Proton.ProtonAuthenticator
 winget install Proton.ProtonVPN
 winget install Git.Git
 winget install OpenJS.NodeJS
+winget install Blur009.BlurAutoClicker
 
 Start-Process "https://github.com/TestcordDev/TestCord/releases/download/latest/Windows_Testcord_installer-rel.exe"
 Start-Process "https://github.com/KloBraticc/Voidstrap/releases/latest/download/Voidstrap.exe"
