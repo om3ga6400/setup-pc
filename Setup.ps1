4d4b4c093045a8bef1fbb4ab930f033e309a21b4
om3ga6400 <om3ga6400.dev@gmail.com>
2026-09-07 17:43:26 -0700

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -17,6 +17,9 @@ winget install OBSProject.OBSStudio
 winget install Devolutions.UniGetUI
 winget install Jurplel.qView
 winget install VideoLAN.VLC
+winget install Proton.ProtonPass
+winget install Proton.ProtonAuthenticator
+winget install Proton.ProtonVPN
 
 Start-Process "https://github.com/TestcordDev/TestCord/releases/download/latest/Windows_Testcord_installer-rel.exe"
 Start-Process "https://github.com/KloBraticc/Voidstrap/releases/latest/download/Voidstrap.exe"
